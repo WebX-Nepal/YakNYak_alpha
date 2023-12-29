b994b5cf713659c71c9f22730ee023587a6e6662
Ankit Shrestha <shresthaankit72@gmail.com>
2023-12-29 15:52:46 +0545

    push

--- src/pages/home/section6/Section6.jsx
+++ src/pages/home/section6/Section6.jsx
@@ -7,6 +7,7 @@ const Section6 = () => {
                 <div className="heading text-center">
                     <h2 className='w-[60%] m-auto font-bold text-[3em] pt-32'>Where to buy</h2>
                     <p className='w-[60%] m-auto mt-8'>Nulla imperdiet ultrices quam, et semper nibh fringilla in. Aenean a porttitor nisl, sed laoreet mauris. Pellentesque fermentum gravida massa, eu ultrices enim tincidunt id.</p>
+                    <h1>hello</h1>
                 </div>
             </div>
         </>
