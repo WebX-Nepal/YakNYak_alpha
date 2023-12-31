739ec5dfe8c15ab3840b7fa16a97130ffb088354
Ankit Shrestha <shresthaankit72@gmail.com>
2023-12-31 19:29:27 +0545

    history

--- src/pages/home/article/RecentArticle.jsx
+++ src/pages/home/article/RecentArticle.jsx
@@ -1,109 +1,69 @@
 import React, { useState } from 'react'
-// import article1 from "../../../assets/article1.jpg"
-// import article2 from "../../../assets/article2.jpg"
-// import article3 from "../../../assets/article3.jpg"
-
+import { data, buttonNames } from "./staticContent";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
-import History1986 from './history1986';
-
-
 
 const RecentArticle = () => {
-    const [initial, setInitial] = useState(false);
+    const [displayYear, setDisplayYear] = useState("1832")
+    function handleSwitches(name) {
+        switch (name) {
 
+            case "1832":
+                setDisplayYear("1832")
+                break;
+            case "1854":
+                setDisplayYear("1854")
+                break;
+            case "1864":
+                setDisplayYear("1864")
+                break;
+            case "1870":
+                setDisplayYear("1870")
+                break;
+            case "1887":
+                setDisplayYear("1887")
+                break;
+            case "1907":
+                setDisplayYear("1907")
+                break;
+
+            case "1930s":
+                setDisplayYear("1930s")
+                break;
+            default:
+                setDisplayYear("2000")
+        }
+    }
 
     return (
         <>
-            <div className="article  w-full">
-                {/* <div className="content pt-24">
-                    <h1 className='text-[3.5em] font-bold text-center' >Recent Articles</h1>
-                    <div className="card flex justify-center gap-20 mt-10">
-                        <div className="item w-[23%] bg-[#f6f3ee] ">
-                            <div className="overflow-hidden ">
-                                <img src={article1}
-                                    alt="articleimgerr" className='hover:scale-105 transition-all duration-300' />
-                            </div>
-                            <div className="details p-6">
-                                <h6 className="text-center">Brewery</h6>
-                                <h4 className="text-[1.5em] font-bold mt-4 text-center">Beer production in China will be the largest in the world in 2023</h4>
-                                <div className="date flex gap-3 mt-4 justify-center text-[grey]">
-                                    <span>January 12,2019</span>
-                                    <hr />
-                                    <span>1 comment</span>
-                                </div>
-                            </div>
-
-
-                        </div>
-                        <div className="item w-[23%] bg-[#f6f3ee]">
-                            <div className="overflow-hidden ">
-                                <img src={article2}
-                                    alt="articleimgerr" className='hover:scale-105 transition-all duration-300' />
-                            </div>
-                            <div className="details p-6">
-                                <h6 className="text-center">Brewery</h6>
-                                <h4 className="text-[1.5em] font-bold mt-4 text-center">Beer production in China will be the largest in the world in 2023</h4>
-                                <div className="date flex gap-3 mt-4 justify-center text-[grey]">
-                                    <span>January 12,2019</span>
-                                    <hr />
-                                    <span>1 comment</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="item w-[23%] bg-[#f6f3ee] ">
-                            <div className="overflow-hidden ">
-                                <img src={article3}
-                                    alt="articleimgerr" className='hover:scale-105 transition-all duration-300' />
-                            </div>
-                            <div className="details p-6">
-                                <h6 className="text-center">Brewery</h6>
-                                <h4 className="text-[1.5em] font-bold mt-4 text-center">Beer production in China will be the largest in the world in 2023</h4>
-                                <div className="date flex gap-3 mt-4 justify-center text-[grey]">
-                                    <span>January 12,2019</span>
-                                    <hr />
-                                    <span>1 comment</span>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-
-                    <div className="button w-full flex justify-center xl:mt-7">
-                        <button className='bg-[#e6bf55] hover:bg-[#272361] py-5 px-20 font-bold rounded-full text-[1.2em] transition-all duration-300 mt-10 hover:text-yellow-500 text-[#272361]'>Read More</button>
-                    </div>
-                </div> */}
+            <div className="article  w-full bg-white">
                 <div className="content">
                     <h1 className='text-center font-bold text-[2em] xl:w-[60%] m-auto pt-24 tracking-wider'>Carlton & United Breweries is the most iconic beer company in Australia, with a history dating back to 1832</h1>
                     <div className="history-year flex gap-5 justify-center mt-10">
-                        <div className="year">
-                            <h5 onClick={() => setInitial(!initial)} className='cursor-pointer  '>1864</h5>
-                            <FontAwesomeIcon icon={faCircle} className='h-2' />
-                        </div>
-                        <div className="year">
-                            <h5>1986</h5>
-                            <FontAwesomeIcon icon={faCircle} className='h-2' />
-                        </div>
-                        <div className="year">
-                            <h5>1986</h5>
-                            <FontAwesomeIcon icon={faCircle} className='h-2' />
-                        </div>
-                        <div className="year">
-                            <h5>1986</h5>
-                            <FontAwesomeIcon icon={faCircle} className='h-2' />
-                        </div>
-                        <div className="year">
-                            <h5>1986</h5>
-                            <FontAwesomeIcon icon={faCircle} className='h-2' />
-                        </div>
+                        {buttonNames.map((item, index) => {
+                            return <div key={index} className="year">
+                                <h5 className='cursor-pointer'
+                                    onClick={() => handleSwitches(item.year)} >{item.year}</h5>
+                                <FontAwesomeIcon icon={faCircle} className='h-2' />
+                            </div>
+                        })}
                     </div>
                     <div className="history-details mt-10">
-                        <h1>
-                            {
-                                initial && <History1986 />
-                            }
-
-                        </h1>
+                        {data.map((item, index) => {
+                            if (displayYear == item.year)
+                                return (<>
+                                    <div key={index} className='history-container '>
+                                        <div className="content w-[40%] p-10 m-auto relative bg-[white] left-24 z-10">
+                                            <h4 className='font-bold text-[1.5em] '>{item.title}</h4>
+                                            <p className='text-[grey] mt-5 text-[1.2em] tracking-wider'>{item.content}</p>
+                                        </div>
+                                    </div>
+                                    <div className="image flex justify-center mt-[-60px] ">
+                                        <img src={item.image} alt="img err" className='xl:h-[700px] xl:w-[1000px] object-cover' />
+                                    </div>
+                                </>)
+                        })}
                     </div>
                 </div>
             </div >
---
+++ src/pages/home/article/staticContent.js
@@ -0,0 +1,56 @@
+import im1864 from "../../../assets/1864.jpg";
+import im1832 from "../../../assets/1832.jpg";
+import im1854 from "../../../assets/1854.jpg";
+import im1870 from "../../../assets/1870.jpg";
+import im1887 from "../../../assets/1887.jpg";
+
+export const buttonNames = [
+  { year: "1832" },
+  { year: "1854" },
+  { year: "1864" },
+  { year: "1870" },
+  { year: "1887" },
+  { year: "1907" },
+  { year: "1930s" },
+];
+
+export const data = [
+  {
+    title: "Thomas Aitken discovers how to quench a hard earned thirst",
+    content:
+      "In 1854 Thomas Aitken founded the Victoria Brewery and became famous for his award winning bitter ales, brewed to provide refreshment in the harsh Australian climate. Aitken's Victoria Bitter Ale quickly became a popular drink for thirsty miners drawn to Victoria during the gold rush.",
+    year: 1854,
+    image: im1854,
+  },
+  {
+    title:
+      "Peter Degraves creates delicious alternative to imported liquor using pristine Tasmanian ingredients",
+    content:
+      "In the early 19th century there were very few breweries in Australia, so people drank expensive imported brands from England or 'rot-gut' liquors said to send men mad. Peter Degraves was a fixer - he devised a way to divert fresh water to Hobart town and even submitted plans for improving a Hobart jail during his incarceration there in 1826. Although these two plans failed, Degraves' plans for a brewery that produced 'genuine beer beer that cannot be excelled in this colony' succeeded and Cascade Brewery Co. was born in 1832. Degraves used the sparkling fresh mountain water from the cascades as well as Tasmania's quality hops to create the first great Australian beer.",
+    year: 1832,
+    image: im1832,
+  },
+  {
+    title:
+      "Immigrant Edward Latham buys a brewery in Carlton and founds an empire",
+    content:
+      "Latham didn't have any brewing experience but he knew a good opportunity when he saw one, so he bought the Bouverie St operation in 1864 and renamed it Carlton Brewery. For his head brewer, Latham hired Gisborne-born Alfred Terry who had the magic touch when it came to brewing palatable, bright and clear beer which preserved well. Terry's lighter colonial beers were much easier to drink in the Australian heat than the heavy European imports and soon barrels of freshly brewed Carlton Ale were being transported by Clydesdale to hotels throughout the city.",
+    year: 1864,
+    image: im1864,
+  },
+  {
+    title: "The Carlton Brewery Fire Brigade becomes a community leader",
+    content:
+      "In the 19th century, most buildings in Melbourne were made of wood and although fires were common, there was no official fire brigade. Carlton Brewery decided to pitch in and quickly developed a brigade made up of volunteer brewery workers. The Carlton Brewery Fire Brigade was one of Melbourne's best and most efficient it's said that they could have an engine horsed and ready within 40 seconds of the fire alarm sounding. The brigade was also innovative brigade leader and head brewer Alfred Terry invented a safer type of 'jump sheet' to break the fall of people jumping out of the windows of burning buildings.",
+    year: 1870,
+    image: im1870,
+  },
+  {
+    title:
+      "The Foster Brothers pioneer the concept of a cold beer on a hot summer day",
+    content:
+      "In 1887, New York-born brothers William and Ralph Foster used an innovative engine-powered refrigeration process to brew Foster's Lager and store it for 60 days at 1.6 degrees Celsius, which aided fermentation. Foster's Lager premiered in Melbourne on 1 February 1889 (the city's hottest month). In an ingenious marketing ploy, any hotel that agreed to sell Foster's received a free supply of ice and the concept of drinking a cold beer on a summer's day (up until this point beer was consumed at room temperature) quickly caught on.",
+    year: 1887,
+    image: im1887,
+  },
+];
